9c1a35ffcfeaeafc213f06f56aba26042a000410
Chvl_Adrien <chvl.adrien@gmail.com>
2021-06-21 14:26:08 +0200

    Create controleMobile.js

---
+++ JS/controleMobile.js
@@ -0,0 +1,20 @@
+class controleMobile extends Phaser.Scene {
+    constructor() {
+        super('controleMobile')
+    }
+
+    preload()
+	{
+
+        
+	}
+
+    create() { 
+                
+
+    }
+
+    update(){
+        this.image.rotation += 0.05;
+    }
+}
