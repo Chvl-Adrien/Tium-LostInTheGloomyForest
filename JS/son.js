ab2f4417f0cfca893870144ae4a91da105147ced
Chvl_Adrien <chvl.adrien@gmail.com>
2021-06-21 14:26:22 +0200

    Create son.js

---
+++ JS/son.js
@@ -0,0 +1,20 @@
+class son extends Phaser.Scene {
+    constructor() {
+        super('son')
+    }
+
+    preload()
+	{
+
+        
+	}
+
+    create() { 
+                
+
+    }
+
+    update(){
+        this.image.rotation += 0.05;
+    }
+}
